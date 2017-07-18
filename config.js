37dddb5a2d93caab3f00a7c8cdba126d8e27f5eb
H5DevHoward <h5devhoward@gmail.com>
2017-07-18 11:50:48 +0800

    initial commit
    权限管理 token验证

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // secret: used when we create and verify JSON Web Tokens
+    'secret': 'ilovescotchyscotch',
+    // database: the URI with username and password to your MongoDB installation
+    // 'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN',
+    'database': 'mongodb://localhost/authenticate-mongoose',
+
+};
